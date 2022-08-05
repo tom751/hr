c7f4b278aa9cb636d89bca47aa5d4225004bc519
Tom Hilton <tomhilton93@gmail.com>
2022-08-05 18:54:42 +0100

    Add express

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,8 +1,13 @@
 import { createServer } from '@graphql-yoga/node'
 import schema from '@/schema'
+import express from 'express'
 
 const server = createServer({
   schema,
 })
 
-server.start()
+const app = express()
+app.use('/graphql', server)
+
+const port = 4000
+app.listen(port, () => console.log(`Running on port http://localhost:${port}/graphql`))
